1a037acf8e5aa9e39f2d6385f4f64b1eeccc4add
Santiago Avendano <santidbd@gmail.com>
2026-06-17 01:12:18 -0400

    quitamos el backend

--- backend/routes/purchase.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createPurchase } from '../controllers/purchase.controller.js';
-
-const router = express.Router();
-
-router.post('/', createPurchase);
-
-export default router;
--- backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getUserPurchases } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-router.get('/:id_usuario/purchases', getUserPurchases);
-
-export default router;
